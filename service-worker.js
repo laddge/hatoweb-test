11c67e93320a313901da81481a0b4c31a5e68811
Laddge <dev.laddge@gmail.com>
2021-05-14 19:53:23 +0900

    deploy

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.1cb9027a499da0a098499a64077a5ace.js"
+  "precache-manifest.4c8c3bc613ce191e9bbab9b89c2a995c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "hatoweb"});
